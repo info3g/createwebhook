2699ff5d256ee39856bbae621856570b71f6ee9e
info3g <infothreeg@gmail.com>
2018-05-02 15:43:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 echo "<h1>welcome to my app</h1>";
-require __DIR__.'/connection.php'; 
 require __DIR__.'/conf.php'; 
 require __DIR__.'/vendor/autoload.php';
  $config = pg_query($dbconn4, "SELECT last_name FROM customers WHERE id = 48");
