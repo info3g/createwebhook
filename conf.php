d409dad7888309698ff30bf222bf1c293a6d9eff
info3g <infothreeg@gmail.com>
2018-05-02 16:42:01 +0530

    Update conf.php

--- conf.php
+++ conf.php
@@ -2,4 +2,5 @@
 //adding
 define('SHOPIFY_APP_API_KEY', 'f3fbece71ca0e9d130649d01c431b831');
 define('SHOPIFY_APP_SHARED_SECRET', '17eb1f939ff7b9608e3d9019ccd6275c');
+define('WEBHOOK_APP_URL','https://createwebhook.herokuapp.com');
 ?>
