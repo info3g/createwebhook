e50a9a8e259527eaf7687528da2d99e9162689de
info3g <infothreeg@gmail.com>
2018-05-02 16:01:29 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,26 @@ ini_set('display_errors', 1);
 echo "<h1>welcome to my app</h1>";
 require __DIR__.'/conf.php'; 
 require __DIR__.'/vendor/autoload.php';
-
+use phpish\shopify;
+if(isset($_REQUEST['shop']))
+{
+  $webhook_url = $_REQUEST['shop']."/admin/webhooks.json";
+  $webhook_data = array('webhook' =>
+    array(
+      'topic' => 'products/update',
+      'address' => WP_SHOP_URL,
+      'format' => 'json'
+    )
+  );
+  $webhookdata = json_encode($webhook_data);
+  $ch = curl_init($webhook_url);
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $webhookdata);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array( 'Content-Type: application/json', 'Content-Length: ' . strlen($webhookdata)));
+  $curlResponse = curl_exec($ch);
+  curl_close($ch);
+  print_r($curlResponse);
+}
 ?>
 
