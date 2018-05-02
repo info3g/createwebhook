84e11daba4e8cdd894650c034f5b9b022e24ae7c
info3g <infothreeg@gmail.com>
2018-05-02 16:24:58 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-echo "<h1>welcome to my app</h1>";
+echo "<h1>welcome to Webhook Auto Creation app</h1>";
 require __DIR__.'/conf.php'; 
 require __DIR__.'/vendor/autoload.php';
 use phpish\shopify;
