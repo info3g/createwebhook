667c5f712f4d16b13135260b31b46614c1a350ca
info3g <infothreeg@gmail.com>
2018-05-02 16:23:50 +0530

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,3 +1,22 @@
 <?php
-mail('samriti.3ginfo@gmail.com', 'Order Create' , 'message sent');
+require __DIR__.'/conf.php'; 
+require __DIR__.'/vendor/autoload.php';
+use phpish\shopify;
+
+function verify_webhook($data, $hmac_header) {
+  $calculated_hmac = base64_encode(hash_hmac('sha256', $data, SHOPIFY_APP_SHARED_SECRET, true));
+  if($hmac_header == $calculated_hmac) {
+	  return true;
+  }
+}
+$hmac_header = $_SERVER['HTTP_X_SHOPIFY_HMAC_SHA256'];
+$data = file_get_contents('php://input');
+$verified = verify_webhook($data, $hmac_header);
+error_log('Webhook verified: '.var_export($verified, true));
+if($verified) {
+  mail('samriti.3ginfo@gmail.com', 'Order Create' , 'message sent');
+  mail('samriti.3ginfo@gmail.com', 'Order Data' , $data);
+	$dataarray[] = json_decode($wdata, true);
+} 
+  
 ?>
